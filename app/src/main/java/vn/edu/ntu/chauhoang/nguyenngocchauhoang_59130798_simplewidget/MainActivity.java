9c6f135337e6e645fbe96bc1f8323136e45886ac
chauhoang99er <63139849+chauhoang99er@users.noreply.github.com>
2020-04-21 22:29:43 +0700

    v0.2

--- app/src/main/java/vn/edu/ntu/chauhoang/nguyenngocchauhoang_59130798_simplewidget/MainActivity.java
+++ app/src/main/java/vn/edu/ntu/chauhoang/nguyenngocchauhoang_59130798_simplewidget/MainActivity.java
@@ -3,12 +3,41 @@ package vn.edu.ntu.chauhoang.nguyenngocchauhoang_59130798_simplewidget;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    EditText edtHoTen, edtNgaySinh, edtSThich;
+    RadioGroup rdgGioiTinh;
+    CheckBox cbXP, cbNN, cbCF, cbONha, cbVB;
+    Button btnXN;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Gọi Phương Thức
+        AddView();
+        AddEvent();
     }
+
+    private void AddView()
+    {
+        edtHoTen = findViewById(R.id.edtHoTen);
+        edtNgaySinh = findViewById(R.id.edtNgaySinh);
+        edtSThich = findViewById(R.id.edtSthich);
+        rdgGioiTinh = findViewById(R.id.rdgGioiTinh);
+        cbXP = findViewById(R.id.cbXP);
+        cbNN = findViewById(R.id.cbNN);
+        cbCF = findViewById(R.id.cbCF);
+        cbONha = findViewById(R.id.cbONha);
+        cbVB = findViewById(R.id.cbVB);
+        btnXN = findViewById(R.id.btnXN);
+    }
+
+    //Sự kiện khi nhấn nút
 }
