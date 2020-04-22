07389657bdabd36104c4a56e9386d3f5933ec8c3
chauhoang99er <63139849+chauhoang99er@users.noreply.github.com>
2020-04-22 10:40:26 +0700

    v0.3

--- app/src/main/java/vn/edu/ntu/chauhoang/nguyenngocchauhoang_59130798_simplewidget/MainActivity.java
+++ app/src/main/java/vn/edu/ntu/chauhoang/nguyenngocchauhoang_59130798_simplewidget/MainActivity.java
@@ -3,10 +3,12 @@ package vn.edu.ntu.chauhoang.nguyenngocchauhoang_59130798_simplewidget;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -40,4 +42,48 @@ public class MainActivity extends AppCompatActivity
     }
 
     //Sự kiện khi nhấn nút
+    private void AddEvent()
+    {
+        btnXN.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                tienHanh();
+            }
+        });
+    }
+
+    private void tienHanh()
+    {
+        String Ketqua = "";
+        Ketqua = edtHoTen.getText() + "\n" +
+                "Ngày sinh: " + edtNgaySinh.getText() + "\n";
+        switch (rdgGioiTinh.getCheckedRadioButtonId())
+        {
+            case R.id.rbNam: Ketqua += "Giới tính: " + "Nam" + "\n" + "Sở thích: "; break;
+            case R.id.rbNu: Ketqua += "Giới tính: " + "Nữ" + "\n" + "Sở thích: "; break;
+        }
+
+        //Gọi hàm thực thi checkbox
+        Ketqua += HDCheckBox();
+        Toast.makeText(MainActivity.this, Ketqua, Toast.LENGTH_SHORT).show();
+    }
+
+    private String HDCheckBox()
+    {
+        String Ketqua="";
+        CheckBox [] arrCb = new CheckBox[]{cbXP, cbNN, cbCF, cbONha, cbVB};
+        for(int i = 0; i < arrCb.length; i++)
+        {
+            if(arrCb[i].isChecked())
+            {
+                Ketqua += arrCb[i].getText().toString() + "; ";
+            }
+        }
+
+        //Thêm sở thích khác vào Ketqua nếu có
+        Ketqua += edtSThich.getText();
+        return Ketqua;
+    }
 }
